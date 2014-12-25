29579867ef585505013efd835d49390f46e01e63
liurong <570899307@qq.com>
2014-12-25 10:51:46 -0500

    test toString in cart-item-tests

--- __tests__/cart-item-tests.js
+++ __tests__/cart-item-tests.js
@@ -1,19 +1,19 @@
 jest.dontMock('../pos_v2_3rdlib/src/model/cart-item.js');
 describe('cart-item', function() {
   var CartItem = require('../pos_v2_3rdlib/src/model/cart-item.js');
-  var cartItem = new CartItem({barcode:'ITEM000000',name:'可口可乐',unit:'瓶',price:3},1);
+  var cartItem = new CartItem({barcode:'ITEM000001',name:'雪碧',unit:'瓶',price:3},1);
 
   describe('#getBarcode', function() {
     it('should return corret barcode', function() {
       var result = cartItem.getBarcode();
-      expect(result).toEqual('ITEM000000');
+      expect(result).toEqual('ITEM000001');
     });
   });
 
   describe('#getPrice', function() {
     it('should return corret price', function() {
       var result = cartItem.getPrice();
-      expect(result).toEqual(3);
+      expect(result).toBe(3);
     });
   });
 
@@ -27,18 +27,24 @@ describe('cart-item', function() {
   describe('#getOriginSubtotal', function() {
     it('should return corret originSubtotal', function() {
       var result = cartItem.getOriginSubtotal();
-      expect(result).toEqual(3);
+      expect(result).toBe(3);
     });
   });
 
   describe('#getSubtotal', function() {
     it('should return corret subtotal', function() {
       var result = cartItem.getSubtotal();
-      expect(result).toEqual(3);
+      expect(result).toBe(3);
     });
   });
 
-
+  describe('#toString', function() {
+    it('should return corret toString', function() {
+      var result = cartItem.toString();
+      var string = '名称：雪碧，数量：1瓶，单价：3.00(元)，小计：3.00(元)';
+      expect(result).toEqual(string);
+    });
+  });
 
 
 
