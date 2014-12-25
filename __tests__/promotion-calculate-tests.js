9c8cb050b58812f9a452a1963e84a3f0619537ae
liurong <570899307@qq.com>
2014-12-25 16:35:04 -0500

    test promotion is calculateCartItems in promotion-calculate-tests

--- __tests__/promotion-calculate-tests.js
+++ __tests__/promotion-calculate-tests.js
@@ -1,32 +1,30 @@
 jest.dontMock('../pos_v2_3rdlib/src/model/promotion-calculator.js');
 jest.dontMock('lodash');
 
-var PromotionCalculator;
-var promotions;
-
-beforeEach(function() {
-  PromotionCalculator = require('../pos_v2_3rdlib/src/model/promotion-calculator.js');
-
-  promotions =
-  [{
-    type: 'BUY_TWO_GET_ONE_FREE',
-    barcodes :
-    ['ITEM000000',
-    'ITEM000001',
-    'ITEM000005']
-  }];
-});
-
-
 describe('PromotionCalculator', function() {
+  var PromotionCalculator,promotions,getBarcode,getPrice;
+
+  beforeEach(function() {
+    PromotionCalculator = require('../pos_v2_3rdlib/src/model/promotion-calculator.js');
+
+    promotions =
+    [{
+      type: 'BUY_TWO_GET_ONE_FREE',
+      barcodes :
+      ['ITEM000000',
+      'ITEM000001',
+      'ITEM000005']
+    }];
+
+    getBarcode = jest.genMockFn();
+    getBarcode.mockReturnValue('ITEM000001');
+
+    getPrice = jest.genMockFn();
+    getPrice.mockReturnValue(3);
+  });
+
   describe(',calculate', function() {
     it('should return corret number', function() {
-      
-      var getBarcode = jest.genMockFn();
-      getBarcode.mockReturnValue('ITEM000001');
-
-      var getPrice = jest.genMockFn();
-      getPrice.mockReturnValue(3);
 
       var cartItem =
           {
@@ -44,6 +42,40 @@ describe('PromotionCalculator', function() {
 
       PromotionCalculator.calculate(cartItem, promotions);
       expect(cartItem.savedCount).toBe(1);
+    });
+  });
+
+  describe(',calculateCartItems', function() {
+    it('should return corret number', function() {
+
+      var cartItems =
+          [{
+            item :
+              {barcode:'ITEM000001',
+              name:'雪碧',
+              unit:'瓶',
+              price:3},
+            count : 5,
+            savedCount : 0,
+            savedPrice : 0,
+            getBarcode : getBarcode,
+            getPrice : getPrice
+          }];
+
+  PromotionCalculator.calculateCartItems(cartItems, promotions);
+  expect(cartItems).toEqual(
+                      [{
+                        item :
+                          {barcode:'ITEM000001',
+                          name:'雪碧',
+                          unit:'瓶',
+                          price:3},
+                        count : 5,
+                        savedCount : 1,
+                        savedPrice : 3,
+                        getBarcode : getBarcode,
+                        getPrice : getPrice
+                      }]);
     });
   });
 });
