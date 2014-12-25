dcd26b61abca249b16d72d220459b19c6793f3a0
liurong <570899307@qq.com>
2014-12-25 16:11:17 -0500

    add beforeEach in the promotion-calculate-tests

--- __tests__/promotion-calculate-tests.js
+++ __tests__/promotion-calculate-tests.js
@@ -1,20 +1,27 @@
 jest.dontMock('../pos_v2_3rdlib/src/model/promotion-calculator.js');
 jest.dontMock('lodash');
 
+var PromotionCalculator;
+var promotions;
+
+beforeEach(function() {
+  PromotionCalculator = require('../pos_v2_3rdlib/src/model/promotion-calculator.js');
+
+  promotions =
+  [{
+    type: 'BUY_TWO_GET_ONE_FREE',
+    barcodes :
+    ['ITEM000000',
+    'ITEM000001',
+    'ITEM000005']
+  }];
+});
+
+
 describe('PromotionCalculator', function() {
   describe(',calculate', function() {
     it('should return corret number', function() {
-      var PromotionCalculator = require('../pos_v2_3rdlib/src/model/promotion-calculator.js');
-
-      var promotions =
-          [{
-            type: 'BUY_TWO_GET_ONE_FREE',
-            barcodes :
-              ['ITEM000000',
-              'ITEM000001',
-              'ITEM000005']
-          }];
-
+      
       var getBarcode = jest.genMockFn();
       getBarcode.mockReturnValue('ITEM000001');
 
