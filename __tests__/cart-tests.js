ef7236f744ebec94938ed490d39654a395d5adf8
liurong <570899307@qq.com>
2014-12-26 09:51:51 -0500

    test toString in the cart-tests

--- __tests__/cart-tests.js
+++ __tests__/cart-tests.js
@@ -1,5 +1,8 @@
 jest.dontMock('../pos_v2_3rdlib/src/model/cart');
+jest.dontMock('../pos_v2_3rdlib/src/model/cart-item');
+
 jest.dontMock('lodash');
+jest.dontMock('moment');
 describe('Cart', function() {
   var Cart,cart;
 
@@ -59,6 +62,50 @@ describe('Cart', function() {
       var result = cart.getSavedAmount();
 
       expect(result).toBe(6);
+    });
+  });
+
+
+  describe('#toString', function() {
+    var moment = require('moment');
+      it('should return string', function() {
+
+      toString = jest.genMockFn();
+      toString.mockReturnValue('名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)');
+
+
+      toPromotionString = jest.genMockFn();
+      toPromotionString.mockReturnValue('名称：雪碧，数量：1瓶');
+
+      getSubtotal = jest.genMockFn();
+      getSubtotal.mockReturnValue(12);
+
+      hasPromotion = jest.genMockFn();
+      hasPromotion.mockReturnValue(true);
+
+      cart.cartItems =[{
+                       savedPrice : 3,
+                       toString : toString,
+                       hasPromotion : hasPromotion,
+                       toPromotionString : toPromotionString,
+                       getSubtotal : getSubtotal}];
+
+      var result = cart.toString();
+
+      var expectText =
+          '***<没钱赚商店>购物清单***\n' +
+          '打印时间：' + moment().format('YYYY年MM月DD日 HH:mm:ss') + '\n' +
+          '----------------------\n' +
+          '名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)\n' +
+          '----------------------\n' +
+          '挥泪赠送商品：\n' +
+          '名称：雪碧，数量：1瓶\n' +
+          '----------------------\n' +
+          '总计：12.00(元)\n' +
+          '节省：3.00(元)\n' +
+          '**********************';
+
+      expect(result).toEqual(expectText);
     });
   });
 
